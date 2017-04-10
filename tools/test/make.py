48ef86730f4ac0ac6d0d12da2872b4af2c85fb3c
Daniel Fekete <danieleff@gmail.com>
2017-04-10 20:27:22 +0200

    Add makefile to automatically compile tests

---
+++ tools/test/make.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+
+projects = ['test/examples/blinkTest/']
+
+variants = os.listdir('makefiles')
+
+for project in projects:
+    print 'Compiling: ' + project
+    subprocesses = {}
+    
+    for variant in variants:
+        
+        path = 'build/' + variant + '/' + project
+        
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        
+        with open(path + '/stdout.txt', 'w') as out, open(path + '/stderr.txt', 'w') as err:
+            subprocesses[variant] = subprocess.Popen(['make', 'VARIANT=' + variant, 'PROJECT=' + project], stdout = out, stderr = err)
+    
+    for variant, p in subprocesses.iteritems():
+        p.wait()
+        
+        if p.returncode == 0:
+            print '.',
+        else:
+            print
+            print variant + 'COMPILATION ERROR ! ' + str(p.returncode)
+
+print
+print 'FINISH'
---
+++ tools/test/platform_to_makefile.py
@@ -0,0 +1,102 @@
+from collections import OrderedDict
+
+root = '../../STM32';
+
+with open(root + '/boards.txt') as file:
+    boards = file.readlines()
+
+with open(root + '/platform.txt') as file:
+    platform = file.readlines()
+
+variants = {}
+variant_nucleos = {}
+variant_uploads = {}
+    
+for line in boards:
+    line = line.strip()
+    
+    if not line or line.startswith('#') or line.startswith('menu'):
+        continue
+        
+    (board, data) = line.split('.', 1)
+    
+    (key, value) = data.split('=', 1)
+    
+    key = key.strip()
+    value = value.strip()
+    
+    if key.startswith('menu.nucleo_board.'):
+        key = key.replace('menu.nucleo_board.', '')
+        if '.' not in key:
+            continue
+
+        (upload, k) = key.split('.', 1)
+        variant_nucleos.setdefault(board + '_' + upload, variants[board])[k] = value
+    
+    elif key.startswith('menu.upload_method.'):
+        key = key.replace('menu.upload_method.', '')
+        if '.' not in key or 'MassStorageMethod' in key or 'DFUUploadMethod2' in key:
+            continue
+
+        (upload, k) = key.split('.', 1)
+        if variant_nucleos:
+            for name, keyvalues in variant_nucleos.iteritems():
+                variant_uploads.setdefault(name + '_' + upload, keyvalues)[k] = value
+        else:
+            variant_uploads.setdefault(board + '_' + upload, variants[board].copy())[k] = value
+    else:
+        variants.setdefault(board, OrderedDict())[key] = value
+    
+
+for variant in variants.keys():
+    upload_exists = False
+    for variant_upload in variant_uploads.keys():
+        if variant in variant_upload:
+            upload_exists = True
+            break
+    if not upload_exists:
+        variant_uploads[variant] = variants[variant]
+
+for (variant, replaces) in variant_uploads.iteritems():
+    replaces = replaces.copy()
+    replaces['build.project_name'] = '$(VARIANT)/$(PROJECT)/out'
+    replaces['object_file'] = '$@'
+    replaces['source_file'] = '$<'
+    replaces['includes'] = '$(INCLUDES)'
+    replaces['object_files'] = '$(OBJECTS)'
+    
+    replaces['runtime.tools.arm-none-eabi-gcc.path'] = '$(PATH_COMPILER)'
+    
+    replaces['runtime.ide.version'] = '10802'
+    replaces['build.path'] = 'build'
+    replaces['build.arch'] = 'STM32GENERIC'
+    replaces['runtime.hardware.path'] = root
+    replaces['build.system.path'] = root + '/system'
+    replaces['build.core.path'] = root + '/cores/arduino'
+    replaces['build.variant.path'] = '$(PATH_VARIANT)'
+
+    with open('makefiles/' + variant, 'w') as file:
+        
+        for key, value in replaces.iteritems():
+            file.write(key + '=' + value + '\n')
+        
+        for line in platform:
+            line = line.strip()
+            
+            if not line or line.startswith('#'):
+                continue
+                
+            for (key, value) in replaces.iteritems():
+                line = line.replace('{' + key + '}', value)
+            
+            (key, value) = line.split('=', 1)
+            
+            key = key.strip()
+            value = value.strip()
+            
+            if value or key not in replaces:
+                replaces[key] = value
+            
+            line = line.replace('"build/{archive_file}"', '')
+            
+            file.write(line + '\n')
