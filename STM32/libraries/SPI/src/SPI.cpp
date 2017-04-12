2c843cd148b2cfe274cb41dbc7c465a9d7df6533
Daniel Fekete <danieleff@gmail.com>
2017-04-12 07:18:54 +0200

    SPI implementation start

---
+++ STM32/libraries/SPI/src/SPI.cpp
@@ -0,0 +1,90 @@
+#include "SPI.h"
+
+SPIClass SPI(SPI1);
+
+void SPIClass::begin() {
+	apb_freq = stm32_apb_clock_freq((void*)spiHandle.Instance);
+
+	spiHandle.Init.Mode = SPI_MODE_MASTER;
+	spiHandle.Init.Direction = SPI_DIRECTION_2LINES;
+	spiHandle.Init.DataSize = SPI_DATASIZE_8BIT;
+	spiHandle.Init.NSS = SPI_NSS_SOFT;
+
+	#ifdef SPI1
+		if (spiHandle.Instance == SPI1) __HAL_RCC_SPI1_CLK_ENABLE();
+	#endif
+	#ifdef SPI2
+		if (spiHandle.Instance == SPI2) __HAL_RCC_SPI2_CLK_ENABLE();
+	#endif
+	#ifdef SPI3
+		if (spiHandle.Instance == SPI3) __HAL_RCC_SPI3_CLK_ENABLE();
+	#endif
+	#ifdef SPI4
+		if (spiHandle.Instance == SPI4) __HAL_RCC_SPI4_CLK_ENABLE();
+	#endif
+	#ifdef SPI5
+		if (spiHandle.Instance == SPI5) __HAL_RCC_SPI5_CLK_ENABLE();
+	#endif
+	#ifdef SPI6
+		if (spiHandle.Instance == SPI6) __HAL_RCC_SPI6_CLK_ENABLE();
+	#endif
+
+	stm32_af_spi_init(spiHandle.Instance, mosiPort, mosiPin, misoPort, misoPin, sckPort, sckPin);
+};
+
+void SPIClass::beginTransaction(SPISettings settings) {
+	if (this->settings.clock == settings.clock
+			&& this->settings.bitOrder == settings.bitOrder
+			&& this->settings.dataMode == settings.dataMode) {
+		return;
+	}
+	this->settings = settings;
+
+	if (settings.clock > apb_freq / 2) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+	} else if (settings.clock > apb_freq / 4) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+	} else if (settings.clock > apb_freq / 8) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+	} else if (settings.clock > apb_freq / 16) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+	} else if (settings.clock > apb_freq / 32) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+	} else if (settings.clock > apb_freq / 64) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+	} else if (settings.clock > apb_freq / 128) {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
+	}  else {
+		spiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+	}
+
+	if(settings.bitOrder == MSBFIRST) {
+		spiHandle.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	} else {
+		spiHandle.Init.FirstBit = SPI_FIRSTBIT_LSB;
+	}
+
+	if((settings.dataMode == SPI_MODE0) || (settings.dataMode == SPI_MODE1)) {
+		spiHandle.Init.CLKPolarity     = SPI_POLARITY_LOW;
+	} else {
+		spiHandle.Init.CLKPolarity     = SPI_POLARITY_HIGH;
+	}
+
+	if((settings.dataMode == SPI_MODE0) || (settings.dataMode == SPI_MODE2)) {
+		spiHandle.Init.CLKPhase        = SPI_PHASE_1EDGE;
+	} else {
+		spiHandle.Init.CLKPhase        = SPI_PHASE_2EDGE;
+	}
+
+	HAL_SPI_Init(&spiHandle);
+}
+
+void SPIClass::setBitOrder(uint8_t bitOrder) {
+	beginTransaction(SPISettings(settings.clock, bitOrder, settings.dataMode));
+}
+void SPIClass::setDataMode(uint8_t dataMode) {
+	beginTransaction(SPISettings(settings.clock, settings.bitOrder, dataMode));
+}
+void SPIClass::setClockDivider(uint8_t clockDevider) {
+	beginTransaction(SPISettings(apb_freq / clockDevider, settings.bitOrder, settings.dataMode));
+}
---
+++ STM32/libraries/SPI/src/SPI.h
@@ -0,0 +1,108 @@
+#ifndef _SPI_H_INCLUDED
+#define _SPI_H_INCLUDED
+
+#include <Arduino.h>
+#include "stm32_gpio_af.h"
+
+// SPI_HAS_TRANSACTION means SPI has
+//   - beginTransaction()
+//   - endTransaction()
+//   - usingInterrupt()
+//   - SPISetting(clock, bitOrder, dataMode)
+#define SPI_HAS_TRANSACTION 1
+
+// SPI_HAS_EXTENDED_CS_PIN_HANDLING means SPI has automatic
+// CS pin handling and provides the following methods:
+//   - begin(pin)
+//   - end(pin)
+//   - setBitOrder(pin, bitorder)
+//   - setDataMode(pin, datamode)
+//   - setClockDivider(pin, clockdiv)
+//   - transfer(pin, data, SPI_LAST/SPI_CONTINUE)
+//   - beginTransaction(pin, SPISettings settings) (if transactions are available)
+// #define SPI_HAS_EXTENDED_CS_PIN_HANDLING 1
+
+// For compatibility with sketches designed for AVR @ 16 MHz
+// need to go from 64MHz to 16 (/4)
+// New programs should use SPI.beginTransaction to set the SPI clock
+#define SPI_CLOCK_DIV2	 2
+#define SPI_CLOCK_DIV4	 4
+#define SPI_CLOCK_DIV8	 8
+#define SPI_CLOCK_DIV16	 16
+#define SPI_CLOCK_DIV32	 32
+#define SPI_CLOCK_DIV64	 64
+#define SPI_CLOCK_DIV128 128
+
+#define SPI_MODE0 0x00
+#define SPI_MODE1 0x01
+#define SPI_MODE2 0x02
+#define SPI_MODE3 0x03
+
+class SPISettings {
+  public:
+    SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode): clock(clock), bitOrder(bitOrder), dataMode(dataMode) {};
+    SPISettings(): clock(0), bitOrder(0), dataMode(0) {};
+    //init(4000000, MSBFIRST, SPI_MODE0);
+
+    uint32_t clock;
+    uint8_t bitOrder;
+    uint8_t dataMode;
+};
+
+
+
+class SPIClass {
+  public:
+    SPIClass(SPI_TypeDef *instance) {
+    	spiHandle.Instance = instance;
+    };
+
+    void begin();
+    void end();
+
+    void beginTransaction(SPISettings settings);
+	void endTransaction();
+
+	void setBitOrder(uint8_t);
+	void setDataMode(uint8_t);
+	void setClockDivider(uint8_t);
+
+    uint8_t transfer(uint8_t data);
+    uint16_t transfer16(uint16_t data);
+    void transfer(uint8_t *buf, size_t count);
+
+  private:
+    uint32_t apb_freq = 0;
+
+    SPISettings settings = {};
+
+    SPI_HandleTypeDef spiHandle = {};
+
+    GPIO_TypeDef *mosiPort = NULL;
+    uint32_t mosiPin = 0;
+    GPIO_TypeDef *misoPort = NULL;
+    uint32_t misoPin = 0;
+    GPIO_TypeDef *sckPort = NULL;
+    uint32_t sckPin = 0;
+};
+
+inline uint8_t SPIClass::transfer(uint8_t data) {
+	if (HAL_SPI_TransmitReceive(&spiHandle, &data, &data, 1, 1000) != HAL_OK) {
+		return 0;
+	}
+	return data;
+}
+inline uint16_t SPIClass::transfer16(uint16_t data) {
+	if (HAL_SPI_TransmitReceive(&spiHandle, (uint8_t*)&data, (uint8_t*)&data, 2, 1000) != HAL_OK) {
+		return 0;
+	}
+	return data;
+}
+
+inline void SPIClass::transfer(uint8_t *buf, size_t count) {
+	HAL_SPI_TransmitReceive(&spiHandle, buf, buf, count, 1000);
+}
+
+extern SPIClass SPI;
+
+#endif
