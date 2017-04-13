5dd88df4ed3c69c7b0033346f5d09713df8ffa72
Daniel Fekete <danieleff@gmail.com>
2017-04-13 07:07:17 +0200

    Show error if the chip series is not defined in boards.txt

--- STM32/cores/arduino/stm32/stm32_def.h
+++ STM32/cores/arduino/stm32/stm32_def.h
@@ -6,38 +6,32 @@
 #ifdef STM32F0
     #include "stm32f0xx.h"
     #include "stm32f0xx_hal.h"
-#endif
-#ifdef STM32F1
+#elif defined(STM32F1)
     #include "stm32f1xx.h"
     #include "stm32f1xx_hal.h"
-#endif
-#ifdef STM32F2
+#elif defined(STM32F2)
     #include "stm32f2xx.h"
     #include "stm32f2xx_hal.h"
-#endif
-#ifdef STM32F3
+#elif defined(STM32F3)
     #include "stm32f3xx.h"
     #include "stm32f3xx_hal.h"
-#endif
-#ifdef STM32F4
+#elif defined(STM32F4)
     #include "stm32f4xx.h"
     #include "stm32f4xx_hal.h"
-#endif
-#ifdef STM32F7
+#elif defined(STM32F7)
     #include "stm32f7xx.h"
     #include "stm32f7xx_hal.h"
-#endif
-#ifdef STM32L0
+#elif defined(STM32L0)
     #include "stm32l0xx.h"
     #include "stm32l0xx_hal.h"
-#endif
-#ifdef STM32L1
+#elif defined(STM32L1)
     #include "stm32l1xx.h"
     #include "stm32l1xx_hal.h"
-#endif
-#ifdef STM32L4
+#elif defined(STM32L4)
     #include "stm32l4xx.h"
     #include "stm32l4xx_hal.h"
+#else
+    #error "STM32XX is not definedin boards.txt"
 #endif
 
 
