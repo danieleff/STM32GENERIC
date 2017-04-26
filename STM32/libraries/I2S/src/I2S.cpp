74a0efbe12e34f7a914dcc086f28b164bb6e271e
Daniel Fekete <danieleff@gmail.com>
2017-04-26 20:49:16 +0200

    Add I2S Master Clock Output

--- STM32/libraries/I2S/src/I2S.cpp
+++ STM32/libraries/I2S/src/I2S.cpp
@@ -1,19 +1,25 @@
 #include "I2S.h"
 #include "stm32_gpio_af.h"
 
+#include "Arduino.h"
+
 I2SClass::I2SClass(SPI_TypeDef *instance) {
     handle.Instance = instance;
 }
 
 I2SClass::I2SClass(SPI_TypeDef *instance, uint8_t sd, uint8_t ws, uint8_t ck) {
     handle.Instance = instance;
+    stm32SetSD(sd);
+    stm32SetWS(ws);
+    stm32SetCK(ck);
+}
 
-    this->sdPort = variant_pin_list[sd].port;
-    this->sdPin = variant_pin_list[sd].pin_mask;
-    this->wsPort = variant_pin_list[ws].port;
-    this->wsPin = variant_pin_list[ws].pin_mask;
-    this->ckPort = variant_pin_list[ck].port;
-    this->ckPin = variant_pin_list[ck].pin_mask;
+I2SClass::I2SClass(SPI_TypeDef *instance, uint8_t sd, uint8_t ws, uint8_t ck, uint8_t mck) {
+    handle.Instance = instance;
+    stm32SetSD(sd);
+    stm32SetWS(ws);
+    stm32SetCK(ck);
+    stm32SetMCK(mck);
 }
 
 uint8_t I2SClass::begin(i2s_mode_t mode, uint32_t sampleRate, uint8_t bitsPerSample) {
@@ -37,7 +43,15 @@ uint8_t I2SClass::begin(i2s_mode_t mode, uint32_t sampleRate, uint8_t bitsPerSam
     if (handle.Instance == SPI6) __HAL_RCC_SPI6_CLK_ENABLE();
     #endif
 
-    stm32AfI2SInit(handle.Instance, sdPort, sdPin, wsPort, wsPin, ckPort, ckPin);
+
+
+    if (useMck) {
+        stm32AfI2SInitWithMck(handle.Instance, sdPort, sdPin, wsPort, wsPin, ckPort, ckPin, mckPort, mckPin);
+        handle.Init.MCLKOutput = I2S_MCLKOUTPUT_ENABLE;
+    } else {
+        stm32AfI2SInit(handle.Instance, sdPort, sdPin, wsPort, wsPin, ckPort, ckPin);
+        handle.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
+    }
 
     if (mode == I2S_PHILIPS_MODE) {
         handle.Init.Standard = I2S_STANDARD_PHILIPS;
@@ -50,7 +64,6 @@ uint8_t I2SClass::begin(i2s_mode_t mode, uint32_t sampleRate, uint8_t bitsPerSam
     }
 
     handle.Init.Mode = I2S_MODE_MASTER_TX;
-    handle.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
     handle.Init.AudioFreq = I2S_AUDIOFREQ_44K;
     handle.Init.CPOL = I2S_CPOL_LOW;
     handle.Init.ClockSource = I2S_CLOCK_PLL;
@@ -101,4 +114,25 @@ void I2SClass::write(uint16_t *data, uint16_t size) {
 
 void I2SClass::write32(uint32_t *data, uint16_t size) {
     HAL_I2S_Transmit(&handle, (uint16_t*)data, size, 1000);
+}
+
+void I2SClass::stm32SetSD(uint8_t sd) {
+    this->sdPort = variant_pin_list[sd].port;
+    this->sdPin = variant_pin_list[sd].pin_mask;
+}
+
+void I2SClass::stm32SetWS(uint8_t ws) {
+    this->wsPort = variant_pin_list[ws].port;
+    this->wsPin = variant_pin_list[ws].pin_mask;
+}
+
+void I2SClass::stm32SetCK(uint8_t ck) {
+    this->ckPort = variant_pin_list[ck].port;
+    this->ckPin = variant_pin_list[ck].pin_mask;
+}
+
+void I2SClass::stm32SetMCK(uint8_t mck) {
+    this->mckPort = variant_pin_list[mck].port;
+    this->mckPin = variant_pin_list[mck].pin_mask;
+    useMck = true;
 }
