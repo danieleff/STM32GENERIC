0250fd74fa0a450df580884d0e7513fe6189fc07
Daniel Fekete <danieleff@gmail.com>
2017-05-03 20:04:13 +0200

    SDIO: Add low level card info / register access methods

--- STM32/libraries/SDIO/src/SDIO.cpp
+++ STM32/libraries/SDIO/src/SDIO.cpp
@@ -9,7 +9,7 @@
 #include "Arduino.h"
 
 uint8_t SDIOClass::readBlocks(uint32_t block, uint8_t* dst, size_t nb) {
-    error = HAL_SD_ReadBlocks(&handle, (uint32_t*)dst, (uint64_t)block * 512, 512, nb);
+    error = HAL_SD_ReadBlocks(&handle, (uint32_t*)dst, (uint64_t)block * getBlockSize(), getBlockSize(), nb);
 
     if (error != SD_OK) {
         return false;
@@ -20,11 +20,35 @@ uint8_t SDIOClass::readBlocks(uint32_t block, uint8_t* dst, size_t nb) {
 }
 
 uint8_t SDIOClass::writeBlocks(uint32_t block, const uint8_t* src, size_t nb) {
-    error = HAL_SD_WriteBlocks(&handle, (uint32_t*)src, (uint64_t)block * 512, 512, nb);
+    error = HAL_SD_WriteBlocks(&handle, (uint32_t*)src, (uint64_t)block * getBlockSize(), getBlockSize(), nb);
 
     if (error != SD_OK) {
         return false;
     }
 
     return HAL_SD_GetStatus(&handle) == SD_TRANSFER_OK;
+}
+
+uint32_t SDIOClass::getBlockSize() {
+    return SDCardInfo.CardBlockSize;
+}
+
+uint64_t SDIOClass::getCapacity() {
+    return SDCardInfo.CardCapacity;
+}
+
+uint8_t SDIOClass::getType() {
+    return SDCardInfo.CardType;
+}
+
+uint16_t SDIOClass::getRCA() {
+    return SDCardInfo.RCA;
+}
+
+HAL_SD_CIDTypedef SDIOClass::getCID() {
+    return SDCardInfo.SD_cid;
+}
+
+HAL_SD_CSDTypedef SDIOClass::getCSD() {
+    return SDCardInfo.SD_csd;
 }
--- STM32/libraries/SDIO/src/SDIO.h
+++ STM32/libraries/SDIO/src/SDIO.h
@@ -11,6 +11,12 @@ class SDIOClass {
     uint8_t readBlocks(uint32_t block, uint8_t* dst, size_t blocks);
     uint8_t writeBlocks(uint32_t block, const uint8_t* src, size_t blocks);
 
+    uint32_t getBlockSize();
+    uint64_t getCapacity();
+    uint8_t getType();
+    uint16_t getRCA();
+    HAL_SD_CIDTypedef getCID();
+    HAL_SD_CSDTypedef getCSD();
 
     SD_HandleTypeDef handle;
     HAL_SD_CardInfoTypedef SDCardInfo;
