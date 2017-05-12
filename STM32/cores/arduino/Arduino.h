bb92877dcd6f6fcd5dd140fca5b01180bcd8b6f9
Daniel Fekete <danieleff@gmail.com>
2017-05-12 19:42:32 +0200

    Arduino.h: Remove function definitions of inline functions
    Including Arduino.h in multiple files caused problems otherwise

--- STM32/cores/arduino/Arduino.h
+++ STM32/cores/arduino/Arduino.h
@@ -101,16 +101,16 @@ void initVariant(void);
 int atexit(void (*func)()) __attribute__((weak));
 
 void pinMode(uint8_t, uint8_t);
-void digitalWrite(uint8_t, uint8_t);
-int digitalRead(uint8_t);
+//void digitalWrite(uint8_t, uint8_t);
+//int digitalRead(uint8_t);
 int analogRead(uint8_t);
 void analogReference(uint8_t mode);
 void analogWrite(uint8_t, int);
 
-unsigned long millis(void);
-unsigned long micros(void);
-void delay(unsigned long);
-void delayMicroseconds(uint32_t us);
+//unsigned long millis(void);
+//unsigned long micros(void);
+//void delay(unsigned long);
+//void delayMicroseconds(uint32_t us);
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout);
 unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout);
 
