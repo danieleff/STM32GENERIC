99fe3918d345d0ac17eca76afd3385ff23d8e64d
ChrisMicro <c.hab@gmx.net>
2017-05-13 12:01:46 +0200

    Blink_allLeds example readability improved

--- STM32/libraries/BoardExamples/examples/BlackF407VE/Blink_allLeds/Blink_allLeds.ino
+++ STM32/libraries/BoardExamples/examples/BlackF407VE/Blink_allLeds/Blink_allLeds.ino
@@ -1,5 +1,8 @@
 /*  
   blink all LEDs 
+
+  remark: both leds are acrive low which means writing LOW 
+          turns the led on, writing HIGH turns the led off
   
   Black407VE ( STM32F407VE )
   http://wiki.stm32duino.com/images/5/5c/STM32_F4VE_SCHEMATIC.PDF
@@ -16,15 +19,18 @@ void setup()
 {
   pinMode(LED0_BUILTIN, OUTPUT);
   pinMode(LED1_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED0_BUILTIN, HIGH); // led off, led is active low
+  digitalWrite(LED1_BUILTIN, HIGH); // led off, led is active low
 }
 
 void loop()
 {
-    digitalWrite(LED0_BUILTIN, HIGH);
+    digitalWrite(LED0_BUILTIN, LOW); // led on
     delay(1000);
-    digitalWrite(LED0_BUILTIN, LOW);
+    digitalWrite(LED0_BUILTIN, HIGH);// led off
     
-    digitalWrite(LED1_BUILTIN, HIGH);
+    digitalWrite(LED1_BUILTIN, LOW); // led on
     delay(1000);
-    digitalWrite(LED1_BUILTIN, LOW); 
+    digitalWrite(LED1_BUILTIN, HIGH); // led off
 }
