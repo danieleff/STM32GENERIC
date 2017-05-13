84770aa317319490bd2cdfe49ccb490b85341f63
Daniel Fekete <danieleff@gmail.com>
2017-05-13 14:52:14 +0200

    Discovery F746: enable external SDRAM, and move heap to there

--- STM32/variants/DISCOVERY_F746NG/variant.cpp
+++ STM32/variants/DISCOVERY_F746NG/variant.cpp
@@ -2,6 +2,13 @@
 
 #include "stm32_gpio.h"
 
+#include "syscalls.h"
+
+extern "C" void preinitVariant() {
+    //Set heap to external SDRAM
+    setHeap((char*)0xC0000000, (char*)(0xC0000000 + 8 * 1024 * 1024));
+}
+
 extern "C" void initVariant() {
 
     //UART1 is connected to ST-Link V2.1 as Virtual Com port on non-default PA9/PB7 pins
