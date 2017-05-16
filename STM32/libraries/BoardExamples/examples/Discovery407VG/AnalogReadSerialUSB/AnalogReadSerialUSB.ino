27631eccfa817675c228515f70d4a06987f218d3
ChrisMicro <c.hab@gmx.net>
2017-05-16 20:06:03 +0200

    baudrate changed to 115200

--- STM32/libraries/BoardExamples/examples/Discovery407VG/AnalogReadSerialUSB/AnalogReadSerialUSB.ino
+++ STM32/libraries/BoardExamples/examples/Discovery407VG/AnalogReadSerialUSB/AnalogReadSerialUSB.ino
@@ -17,7 +17,7 @@
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  SerialUSB.begin(9600);
+  SerialUSB.begin(115200);
 }
 
 // the loop routine runs over and over again forever:
