892d07fdda91acadc7a41d80e2a623eb480bc24b
Daniel Fekete <danieleff@gmail.com>
2017-05-24 07:09:26 +0200

    upload: fix timeout when waiting for serial

--- tools/win/wait_for_serial.bat
+++ tools/win/wait_for_serial.bat
@@ -1,7 +1,7 @@
 echo Waiting for %1 serial...
 
 for /l %%x in (1, 1, 40) do (
-  ping -w 100 -n 1 127.0.0.1 > nul
+  ping -w 100 -n 1 192.0.2.1 > nul
   mode %1 > nul
   if ERRORLEVEL 0 goto comPortFound
 )
