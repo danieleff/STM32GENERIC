3f32b7e41797944cfb81394e7971ca7df339b770
Daniel Fekete <danieleff@gmail.com>
2017-06-15 07:21:16 +0200

    spi: Add LL SPI

---
+++ STM32/cores/arduino/stm32_HAL/stm32XXxx_ll_spi.h
@@ -0,0 +1,29 @@
+#include "stm32_build_defines.h"
+
+#ifdef STM32F0
+    #include "stm32f0xx_ll_spi.h"
+#endif
+#ifdef STM32F1
+    #include "stm32f1xx_ll_spi.h"
+#endif
+#ifdef STM32F2
+    #include "stm32f2xx_ll_spi.h"
+#endif
+#ifdef STM32F3
+    #include "stm32f3xx_ll_spi.h"
+#endif
+#ifdef STM32F4
+    #include "stm32f4xx_ll_spi.h"
+#endif
+#ifdef STM32F7
+    #include "stm32f7xx_ll_spi.h"
+#endif
+#ifdef STM32L0
+    #include "stm32l0xx_ll_spi.h"
+#endif
+#ifdef STM32L1
+    #include "stm32l1xx_ll_spi.h"
+#endif
+#ifdef STM32L4
+    #include "stm32l4xx_ll_spi.h"
+#endif
