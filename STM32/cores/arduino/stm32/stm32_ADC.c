81d9023ae4175d4e5de7d5e903319478c521b2e6
Daniel Fekete <danieleff@gmail.com>
2017-08-27 16:42:39 +0200

    adc: add debug messages

--- STM32/cores/arduino/stm32/stm32_ADC.c
+++ STM32/cores/arduino/stm32/stm32_ADC.c
@@ -34,6 +34,7 @@
  */
 
 #include "stm32_gpio_af.h"
+#include "stm32_debug.h"
 
 void stm32_adc_init(ADC_HandleTypeDef *handle);
 
@@ -139,8 +140,11 @@ int analogRead(uint8_t pin) {
             handle[instanceIndex].Init.Resolution = ADC_RESOLUTION_12B;
         #endif
 
-        HAL_ADC_Init(&handle);
-
+        HAL_StatusTypeDef error = HAL_ADC_Init(&handle[instanceIndex]);
+        if (error != HAL_OK) {
+            PRINT_ERROR("HAL_ADC_Init failed, error: %d", error);
+            return 0;
+        }
     }
 
     ADC_ChannelConfTypeDef sConfig;
@@ -163,13 +167,27 @@ int analogRead(uint8_t pin) {
         #error "unknown sampleing time"
     #endif
 
-    HAL_ADC_ConfigChannel(&handle[instanceIndex], &sConfig);
-
-    HAL_ADC_Start(&handle[instanceIndex]);
-
-    if (HAL_ADC_PollForConversion(&handle[instanceIndex], 1000) != HAL_OK) {
-            return 0;
+    HAL_StatusTypeDef error = HAL_ADC_ConfigChannel(&handle[instanceIndex], &sConfig);
+    if (error != HAL_OK) {
+        PRINT_ERROR("HAL_ADC_ConfigChannel failed, error: %d", error);
+        return 0;
     }
 
-    return (HAL_ADC_GetValue(&handle[instanceIndex]) << readResolution) >> 12;
+    error = HAL_ADC_Start(&handle[instanceIndex]);
+    if (error != HAL_OK) {
+        PRINT_ERROR("HAL_ADC_Start failed, error: %d", error);
+        return 0;
+    }
+
+    error = HAL_ADC_PollForConversion(&handle[instanceIndex], 1000);
+    if (error != HAL_OK) {
+        PRINT_ERROR("HAL_ADC_PollForConversion failed, error: %d", error);
+        return 0;
+    }
+
+    int ret = (HAL_ADC_GetValue(&handle[instanceIndex]) << readResolution) >> 12;
+
+    HAL_ADC_Stop(&handle[instanceIndex]);
+
+    return ret;
 }
