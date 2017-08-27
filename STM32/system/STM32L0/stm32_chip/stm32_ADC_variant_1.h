0531a2eba1ca50f5823b344ca16f0ee6bbfd45c9
Daniel Fekete <danieleff@gmail.com>
2017-08-27 16:44:25 +0200

    adc: F0/L0/L1 Fix typo of ADC_TypeDef* from ADC to ADC1

--- STM32/system/STM32L0/stm32_chip/stm32_ADC_variant_1.h
+++ STM32/system/STM32L0/stm32_chip/stm32_ADC_variant_1.h
@@ -1,6 +1,6 @@
 const stm32_chip_adc1_channel_type chip_adc1_channel[] = {
-    { ADC, GPIOA, GPIO_PIN_0  , ADC_CHANNEL_0  }, 
-    { ADC, GPIOA, GPIO_PIN_1  , ADC_CHANNEL_1  }, 
-    { ADC, GPIOA, GPIO_PIN_4  , ADC_CHANNEL_4  }, 
-    { ADC, GPIOA, GPIO_PIN_7  , ADC_CHANNEL_7  }, 
+    { ADC1, GPIOA, GPIO_PIN_0  , ADC_CHANNEL_0  }, 
+    { ADC1, GPIOA, GPIO_PIN_1  , ADC_CHANNEL_1  }, 
+    { ADC1, GPIOA, GPIO_PIN_4  , ADC_CHANNEL_4  }, 
+    { ADC1, GPIOA, GPIO_PIN_7  , ADC_CHANNEL_7  }, 
 };
