91cdae8796a3b0c119f85a4dadfae1e8e1e4bf68
ChrisMicro <christoph@roboterclub-freiburg.de>
2018-03-29 08:55:56 +0200

    sinWave demo for F407 corrected

--- STM32/libraries/BoardExamples/examples/BlackF407VE/I2S_MAX98357A_sineWave/I2S_MAX98357A_sineWave.ino
+++ STM32/libraries/BoardExamples/examples/BlackF407VE/I2S_MAX98357A_sineWave/I2S_MAX98357A_sineWave.ino
@@ -5,7 +5,9 @@
   microcontroller.
 
   Hardware:
-   microcontroller board: STM32F407VE Black
+   microcontroller board: STM32F407VE (  512MB flash )
+                          STM32F407VG ( 1024MB flash )
+
    amplifier:             Adafruit I2S amplifier with the MAX98357a chip
                           https://www.adafruit.com/product/3006
 
@@ -15,17 +17,18 @@
   ws  = LRC  // left/right channel
   ck  = SCLK // serial data clock
 
-  MAY 2017 ChrisMicro
+  MARCH 2018 ChrisMicro
 
 */
 #include "I2S.h"
 
 // SPI2 pins are used for I2S.
 // SPI2 is avalailable on the BalckF407VE and does not conflict with peripherals like SDIO
-I2SClass I2S(SPI2, PC3 /*DIN*/ , PB12 /*LRC*/, PB10 /*SCLK*/);
+I2SClass I2S(SPI2, PB15 /*SD-DIN*/ , PB12 /*WS-LRC*/, PB13 /*CK-SCLK*/);
+//I2SClass I2S(SPI2, PC3 /*SD-DIN*/ , PB12 /*WS-LRC*/, PB10 /*CK-SCLK*/);
 
 #define SAMPLINGFREQUENCY 44100
-#define WAVEBUFFERLENGTH  SAMPLINGFREQUENCY * 1 // 1 seconds
+#define NUMBEROFSAMPLES  SAMPLINGFREQUENCY * 1 // 1 seconds
 
 #define DAC_MAX_AMPLITUDE 0x2000 // experimental maximum on my speakers without distortion
 
@@ -36,13 +39,13 @@ void setup()
 
 void loop()
 {
-    for(int n=0; n<WAVEBUFFERLENGTH; n++) {
-        const float   frequency = 440;
-        const float   amplitude = DAC_MAX_AMPLITUDE;
-        int16_t val = ( sin( 2 * PI * frequency / SAMPLINGFREQUENCY * n )) * amplitude;
-        I2S.write(val); //left
-        I2S.write(val); //right
-        n++;
-    }
-    delay(3000);
+  for (int n = 0; n < NUMBEROFSAMPLES; n++) {
+    const float   frequency = 440;
+    const float   amplitude = DAC_MAX_AMPLITUDE;
+    int16_t val = ( sin( 2 * PI * frequency / SAMPLINGFREQUENCY * n )) * amplitude;
+    // the MAX98357a is only single channel. Writting both channels increases the resulting loudness
+    I2S.write(val); //left
+    I2S.write(val); //right
+  }
+  delay(3000);
 }
